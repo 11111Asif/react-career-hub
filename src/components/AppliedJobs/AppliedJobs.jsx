3ddc8535ed3ba4cac4c5409ba04b5ee683be6665
11111Asif <asif040502@gmail.com>
2025-05-18 23:37:02 -0700

    added to applied

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getStoredJobApplication } from "../../Utility/LocalStorage";
+import { IoLocationOutline } from "react-icons/io5";
+import { AiOutlineDollarCircle } from "react-icons/ai";
 
 
 const AppliedJobs = () => {
@@ -9,19 +11,19 @@ const AppliedJobs = () => {
     const [displayJobs, setDisplayJobs] = useState([])
     // console.log(jobs)
 
-    const handleJobsFilter = filter =>{
-        if(filter === 'all'){
+    const handleJobsFilter = filter => {
+        if (filter === 'all') {
             setDisplayJobs(appliedJobs)
         }
-        else if(filter === 'remote'){
+        else if (filter === 'remote') {
             const remoteJobs = appliedJobs.filter(job =>
-                 job.remote_or_onsite === 'Remote')
-                    setDisplayJobs(remoteJobs)
+                job.remote_or_onsite === 'Remote')
+            setDisplayJobs(remoteJobs)
         }
-        else if(filter === 'onsite'){
+        else if (filter === 'onsite') {
             const onsiteJobs = appliedJobs.filter(job =>
-                 job.remote_or_onsite === 'Onsite')
-                 setDisplayJobs(onsiteJobs)
+                job.remote_or_onsite === 'Onsite')
+            setDisplayJobs(onsiteJobs)
         }
     }
 
@@ -49,17 +51,38 @@ const AppliedJobs = () => {
             <details className="dropdown">
                 <summary className="m-1 btn">open or close</summary>
                 <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                    <li onClick={()=>handleJobsFilter('all')}><a>All</a></li>
-                    <li onClick={()=>handleJobsFilter('remote')}><a>Remote</a></li>
-                    <li onClick={()=>handleJobsFilter('onsite')}><a>Onsite</a></li>
+                    <li onClick={() => handleJobsFilter('all')}><a>All</a></li>
+                    <li onClick={() => handleJobsFilter('remote')}><a>Remote</a></li>
+                    <li onClick={() => handleJobsFilter('onsite')}><a>Onsite</a></li>
                 </ul>
             </details>
             <ul>
                 {
                     displayJobs.map(job => <li key={job.id}>
-                        <img src={job.logo} alt="" />
-                        <span>{job.job_title}</span>
+                        <div className="flex mb-8 border-2 w-auto rounded-md bg-slate-100">
+                            <img className="py-5 px-5 bg-[#f4f4f4]" src={job.logo} alt="" />
+                            <div className="py-5 px-5 ">
+                                <span className="text-2xl ml-5">{job.job_title}</span>
+                                <p className="text-xl ml-5">{job.company_name}</p>
+
+                                <div className="flex gap-6 mt-3 ml-5">
+                                    <button className="w-[110px] h-[40px] border-2 border-[#7e90fe] text-[#7e90fe]">{job.remote_or_onsite}</button>
+                                    <button className="w-[110px] h-[40px] border-2 border-[#7e90fe] text-[#7e90fe]">{job.job_type}</button>
+                                </div>
+                                <div className="flex gap-10 mt-3 ml-5">
+                                    <p className="flex gap-3"><IoLocationOutline className="text-2xl"></IoLocationOutline>{job.location}</p>
+                                    <p className="flex gap-3"><AiOutlineDollarCircle className="text-2xl"></AiOutlineDollarCircle>{job.salary}</p>
+                                </div>
+                                <div className="ml-5 mt-3 mb-3">
+                                    <button className="bg-[#9873ff] text-white w-[181px] h-[50px] rounded-md">View Details</button>
+                                </div>
+                            </div>
+
+
+                        </div>
+
                     </li>)
+
                 }
             </ul>
         </div>
--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -3,6 +3,9 @@ import { ToastContainer, toast } from 'react-toastify';
 import { saveJobApplication } from "../../Utility/LocalStorage";
 import { LuBaggageClaim } from "react-icons/lu";
 import { AiOutlineDollarCircle } from "react-icons/ai";
+import { FiPhone } from "react-icons/fi";
+import { MdOutlineEmail } from "react-icons/md";
+import { CiLocationOn } from "react-icons/ci";
 
 const JobDetails = () => {
     const jobs = useLoaderData()
@@ -18,21 +21,29 @@ const JobDetails = () => {
 
     return (
         <div>
-            <h2>Job Details of: {id}</h2>
+            <h2 className="text-4xl mb-6 mt-6 text-center">Job Details of</h2>
             <div className="grid gap-5 md:grid-cols-5">
                 <div className="border md:col-span-3">
-                    <p className="ml-8 mr-8"><span className="text-white font-extrabold text-2xl">Job Description:</span> {job.job_description}</p>
-                    <p className="ml-8 mr-8"><span className="text-white font-extrabold text-2xl">job_responsibility:</span> {job.job_responsibility}</p>
-                    <p className="ml-8 mr-8"><span className="text-white font-extrabold text-2xl">educational_requirements:<br /></span> {job.educational_requirements}</p>
-                    <p className="ml-8 mr-8"><span className="text-white font-extrabold text-2xl">experiences: <br /></span> {job.experiences}</p>
+                    <div className="mt-5">
+                        <p className="ml-8 mr-8"><span className="text-white font-extrabold text-2xl">Job Description:</span> {job.job_description}</p>
+                        <p className="ml-8 mr-8"><span className="text-white font-extrabold text-2xl">job_responsibility:</span> {job.job_responsibility}</p>
+                        <p className="ml-8 mr-8"><span className="text-white font-extrabold text-2xl">educational_requirements:<br /></span> {job.educational_requirements}</p>
+                        <p className="ml-8 mr-8"><span className="text-white font-extrabold text-2xl">experiences: <br /></span> {job.experiences}</p>
+                    </div>
                 </div>
                 <div className="border md:col-span-2">
-                    <div className="ml-8 mr-8">
-                        <h2 className="text-3xl border-b-2">Jobs Details</h2>
+                    <div className="ml-8 mr-8 mt-5">
+                        <h2 className="text-3xl ">Jobs Details</h2>
+                        <p className="border-b-2 mt-3 mb-3"></p>
                         <p className="text-xl flex"><AiOutlineDollarCircle className="mt-1.5"></AiOutlineDollarCircle><span className="font-semibold text-white ml-2">salary: </span>  {job.salary}</p>
                         <p className="text-xl flex"><LuBaggageClaim className="mt-1.5"></LuBaggageClaim><span className="font-semibold text-white ml-2">Job title: </span>  {job.job_title}</p>
+                        <h4 className="text-2xl mt-3">Contact Information</h4>
+                        <p className="border-b-2 mt-5 mb-3"></p>
+                        <p className="flex text-xl"><FiPhone className="mt-1.5"></FiPhone><span className="font-extrabold text-white ml-2">Phone: </span> {job.contact_information.phone}</p>
+                        <p className="flex text-xl"><MdOutlineEmail className="mt-1.5"></MdOutlineEmail><span className="font-extrabold text-white ml-2">Email: </span> {job.contact_information.email}</p>
+                        <p className="flex text-xl"><CiLocationOn className="mt-1 text-3xl"></CiLocationOn><span className="font-extrabold text-white ml-2">Address: </span> {job.contact_information.address}</p>
                     </div>
-                    <button onClick={handleApplyJob} className="btn btn-primary w-full">Apply Now</button>
+                    <button onClick={handleApplyJob} className="btn btn-primary w-full mb-5 mt-4">Apply Now</button>
                 </div>
             </div>
             <ToastContainer />
